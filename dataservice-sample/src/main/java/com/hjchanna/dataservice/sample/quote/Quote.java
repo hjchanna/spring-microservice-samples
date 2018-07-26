8feb10e054b1152cac6ffbff4533f5dfdc6c58ec
Channa Jayamuni <hjchanna@gmail.com>
2018-07-26 15:43:21 +0100

    dataservice sample is created

---
+++ dataservice-sample/src/main/java/com/hjchanna/dataservice/sample/quote/Quote.java
@@ -0,0 +1,57 @@
+package com.hjchanna.dataservice.sample.quote;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ChannaJ
+ */
+@Entity(name = "quote")
+@Table(name = "quote")
+public class Quote implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "quote")
+    private String quote;
+
+    public Quote() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+}
