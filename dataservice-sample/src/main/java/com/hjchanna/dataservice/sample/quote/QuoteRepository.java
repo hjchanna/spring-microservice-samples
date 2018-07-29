cdd8a49fa2ff25aae8df535efb83fc6ccdca8f67
Channa Jayamuni <hjchanna@gmail.com>
2018-07-29 13:03:44 +0100

    configured for IntelliJ

--- dataservice-sample/src/main/java/com/hjchanna/dataservice/sample/quote/QuoteRepository.java
+++ dataservice-sample/src/main/java/com/hjchanna/dataservice/sample/quote/QuoteRepository.java
@@ -1,10 +1,10 @@
 package com.hjchanna.dataservice.sample.quote;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- *
  * @author ChannaJ
  */
 public interface QuoteRepository extends JpaRepository<Quote, Integer> {
--- dataservice-sample/src/main/java/com/hjchanna/dataservice/sample/quote/QuoteResource.java
+++ dataservice-sample/src/main/java/com/hjchanna/dataservice/sample/quote/QuoteResource.java
@@ -2,6 +2,7 @@ package com.hjchanna.dataservice.sample.quote;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
  * @author ChannaJ
  */
 @RestController
