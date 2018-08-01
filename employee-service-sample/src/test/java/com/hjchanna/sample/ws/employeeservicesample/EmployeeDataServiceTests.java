8a3b5ee47ea47ca763be6f0c4ff1b98a4b9b15fc
Channa Jayamuni <hjchanna@gmail.com>
2018-08-01 03:18:41 +0100

    maven parent project configured and removed intellij files

--- employee-service-sample/src/test/java/com/hjchanna/sample/ws/employeeservicesample/EmployeeDataServiceTests.java
+++ employee-service-sample/src/test/java/com/hjchanna/sample/ws/employeeservicesample/EmployeeDataServiceTests.java
@@ -1,12 +1,20 @@
 package com.hjchanna.sample.ws.employeeservicesample;
 
+import com.hjchanna.sample.ws.EmployeeDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EmployeeDataServiceTests {
 
 	@Test
