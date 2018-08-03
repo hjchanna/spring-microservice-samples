8ecba6f4d4071a133a40d2a728178ed75c7f838a
Channa Jayamuni <hjchanna@gmail.com>
2018-08-03 17:43:03 +0100

    test configured in product data service

--- employee-service-sample/src/test/java/com/hjchanna/sample/ws/EmployeeDataServiceTests.java
+++ employee-service-sample/src/test/java/com/hjchanna/sample/ws/EmployeeDataServiceTests.java
@@ -1,21 +1,15 @@
 package com.hjchanna.sample.ws;
 
-import com.hjchanna.sample.ws.EmployeeDataService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@Profile("test")
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class EmployeeDataServiceTests {
 
