d1324351cbb350abcd99ca649d6f162d62201351
Channa Jayamuni <hjchanna@gmail.com>
2018-08-04 20:49:07 +0100

    travis-ci git tag create issue resolved

--- build/tag-github.sh
+++ build/tag-github.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export GITHUB_TOKEN="b58fec28a7f7ded72e67656052640235e11074db"
 export TARGET_URL="https://api.github.com/repos/hjchanna/spring-microservice-samples/releases?access_token=$GITHUB_TOKEN"
 
 body="{
