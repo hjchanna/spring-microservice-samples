7320e4317016f43cce4102e49bb5294b0ddc5d5e
Channa Jayamuni <hjchanna@gmail.com>
2018-08-10 13:59:33 +0100

    refactor to yml config

--- cloud-service-samples/eureka-service-sample/src/main/java/com/hjchanna/sample/cs/eureka/EurekaService.java
+++ cloud-service-samples/eureka-service-sample/src/main/java/com/hjchanna/sample/cs/eureka/EurekaService.java
@@ -2,14 +2,10 @@ package com.hjchanna.sample.cs.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaServer
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EurekaService {
 
     public static void main(String[] args) {
