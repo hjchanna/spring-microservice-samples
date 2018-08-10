55ab6ac94a893bb306ff6e49b9f2a970523694b2
Channa Jayamuni <hjchanna@gmail.com>
2018-08-10 20:46:00 +0100

    configured eureka for dev environment

--- data-service-samples/employee-service-sample/src/main/java/com/hjchanna/sample/ds/EmployeeDataService.java
+++ data-service-samples/employee-service-sample/src/main/java/com/hjchanna/sample/ds/EmployeeDataService.java
@@ -2,10 +2,12 @@ package com.hjchanna.sample.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableDiscoveryClient
 public class EmployeeDataService {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeDataService.class, args);
--- data-service-samples/product-service-sample/src/main/java/com/hjchanna/sample/ds/ProductDataService.java
+++ data-service-samples/product-service-sample/src/main/java/com/hjchanna/sample/ds/ProductDataService.java
@@ -2,8 +2,10 @@ package com.hjchanna.sample.ds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductDataService {
 
     public static void main(String[] args) {
